07ab65c94451a5fd063a2e40ae93eba9d547a5ac
miguelfdzTorre <miguelofdz@gmail.com>
2025-06-03 14:35:26 +0200

    cambios martes2

---
+++ app.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const projects = document.querySelectorAll(".project");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    projects.forEach(project => {
+        observer.observe(project);
+    });
+});
