53e715f7783acf85c96bb513eafe83f36e32b1a6
Aymee <areinar31@gmail.com>
2025-06-03 15:05:47 +0200

    arreglando el if else del javascript

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const texto = document.querySelector(".textoPersona");
   const boton = document.getElementById("toggleResumen");
 
-  if (texto && boton) {
-    boton.addEventListener("click", function () {
-      texto.classList.toggle("expandido");
-      boton.textContent = texto.classList.contains("expandido") ? "Ver menos" : "Ver más";
-    });
-  }
+ if (texto && boton) {
+  boton.addEventListener("click", function () {
+    texto.classList.toggle("expandido");
+
+    if (texto.classList.contains("expandido")) {
+      boton.textContent = "Ver menos";
+    } else {
+      boton.textContent = "Ver más";
+    }
+  });
+}
 });
